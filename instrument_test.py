8f9403c8ae48b08df65c454c04ff6dd0301ca53d
KFNT-Superfluid <kfnt.superfluid@gmail.com>
2025-01-15 15:17:20 +0100

    Create instrument_test.py

---
+++ instrument_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 16:02:58 2023
+
+@author: filip
+"""
+
+import matplotlib.pyplot as plt
+import pyvisa as visa
+import numpy as np
+import os
+import time
+from time import strftime, localtime
+import scipy.optimize as optim
+
+from tqdm import tqdm
+
+import sys
+sys.path.append('C:/Software')
+
+from instruments.SR844 import SR844
+from instruments.Rigol_DG import Rigol_DG
+
+
+def res(f, A, f0, w, phi, b1, b2):
+    bg = b1*(f - f0) + b2
+    peak = A*f*w/(f0**2 - f**2 + 1j*f*w)*np.exp(-1j*phi)
+    return np.abs(bg + peak)
+
+def phase(f, A, f0, w, phi, b1, b2):
+    peak = A*f*w/(f0**2 - f**2 + 1j*f*w)
+    return np.angle(peak)
+
+
+rm = visa.ResourceManager()
+
+
+try:
+    lockin = SR844(rm, 'GPIB0::1::INSTR')
+    print(lockin.harmonic(1))
+    print(lockin.get_settings())               
+finally:
+    pass
+
+    # lockin.close()
+
+    # rm.close()
+        
+        
+
+
+
